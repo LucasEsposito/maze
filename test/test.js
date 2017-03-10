8f1d4f6de4e2d0f39c75ee059b355b0f843e48d5
LucasEsposito <espositolucas95@gmail.com>
2017-03-10 03:52:47 -0300

    project structure, test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+"use strict";
+let assert = require('assert');
+// let losJSHechosPorMi = require('../');
+
+
+describe('TODO: remove this test', function(){
+    it('1 equals 1', function(){
+        assert.equal(1,1);
+    });
+});
