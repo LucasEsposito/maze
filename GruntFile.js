44a25d81aca6c2c6dcdadea27df8c6d6441d8b53
LucasEsposito <espositolucas95@gmail.com>
2017-03-10 02:04:07 -0300

    Set up travis and coveralls

---
+++ GruntFile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        mocha_istanbul: {
+            src: []
+            options: {
+                coverage: true,
+                root: ".",
+                reportFormats: ["lcovonly"]
+            }
+        },
+        coveralls: {
+            options: {
+                force: true
+            },
+            main_target: {
+                src: "coverage/lcov.info"
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks("grunt-coveralls");
+    grunt.loadNpmTasks("grunt-mocha-istanbul");
+    
+    grunt.registerTask("default", "mocha_istanbul");
+    grunt.registerTask("mocha", "mocha_istanbul");
+};
