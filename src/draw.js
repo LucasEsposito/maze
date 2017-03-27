572b01a1b05a0e846668ca83a2124904ca60559d
LucasEsposito <espositolucas95@gmail.com>
2017-03-26 23:17:26 -0300

    block display

--- src/draw.js
+++ src/draw.js
@@ -22,38 +22,37 @@ function draw(){
   //   imageY[countResources] = auxiliaryPosition.y;
   //   countResources++;
   // }
-  images[0]=new Image();
-  images[0].src ='/home/lucas/workspace/maze/static/img/block/10.png';
-  imageX[0]=0;
-  imageY[0]=0;
+  // images[0]=new Image();
+  // images[0].src ='/home/lucas/workspace/maze/static/img/block/10.png';
+  // imageX[0]=0;
+  // imageY[0]=0;
   canvas = document.getElementById('gameScreen');
   if(canvas.getContext){
     context = canvas.getContext('2d');
-    let img = new Image()
-    //let wall = new Wall(10); //por alguna razon no puedo declarar nada aca que no sea propio del lenguaje.
-    img.src = '/home/lucas/workspace/maze/static/img/block/10.png';
-    //img.addEventListener('load', function(){}, false);
-    //img.onload = function(){
-    //drawImageAtPosition(img, 0,3, context);
-      ///context.drawImage(img, 32, 0);
-
-      /*
-      */
-
-      img.onload = function(){
-        canvas = document.getElementById('gameScreen');
-        context = canvas.getContext('2d');
-        context.drawImage(img, 32, 0);};
-      for(let $index in images){
-        context.drawImage(images[$index], imageX[$index], imageY[$index]);
+    for(let i = 0; i < canvas.width/IMAGE_SIZE; i++){
+      for(let j = 0; j < canvas.height/IMAGE_SIZE; j++){
+        drawNewImageAt('/home/lucas/workspace/maze/static/img/block/10.png',i,j);
       }
-      let x = {a:2};
-      let y = x.a;
-
-
-      context.drawImage(img, 64, 0);
-    //};
+    }
   }else{
-
+    alert("Canvas not supported!");
   }
+}
+
+function drawNewImageAt(src,x,y){
+  drawImageAt(createImage(src),x,y);
+}
+
+function createImage(src){
+  let image = new Image();
+  image.src = src;
+  return image;
+}
+
+function drawImageAt(image, x,y){
+  let canvas = document.getElementById('gameScreen');
+  let context = canvas.getContext('2d');
+  context.beginPath();
+  context.drawImage(image, x*32, y*32);
+  context.closePath();
 }
